b19902e1465ff7937fb7569b40b3b7229f9430aa
Pravini Wick <85413981+Pravini15@users.noreply.github.com>
2023-02-03 23:45:02 +0530

    Today summary

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
-const cors = require("cors");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 
 const app = express();
---
+++ book-store/src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AppBar, Toolbar, Tabs, Typography, Tab } from "@mui/material";
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+    const [value, setValue] = useState();
+  return (
+    <div>
+        <AppBar sx={{ backgroundColor:'#232F3D'}} position="sticky">
+           <Toolbar> 
+            <Typography>
+                <AutoStoriesOutlinedIcon/>
+            </Typography>
+            <Tabs 
+                 sx = {{ml:'auto'}}
+                 textColor='inherit' 
+                 indicatorColor="primary" 
+                 value={value} onChange={(e,val)=>setValue(val)}
+                 >
+                 <Tab LinkComponent={NavLink} to="/add" label='Add product'/>
+                 <Tab LinkComponent={NavLink} to="/books" label='Books'/>
+                 <Tab LinkComponent={NavLink} to="/aboutus" label='About Us'/>
+            </Tabs>
+           </Toolbar>
+        </AppBar>   
+    </div>
+  )
+}
+
+export default Header;
